11ab63f4ad6a9557048ed9fbdae45302b0267fa2
Ben Tran <66916399+7ben18@users.noreply.github.com>
2021-10-10 20:44:36 +0200

    update

--- 01AufgabenSerien_primzahl/test.py
+++ 01AufgabenSerien_primzahl/test.py
@@ -7,3 +7,13 @@ zipped_iterator = zip(names, powers, marks)
 # Converting iterator to list
 zipped_values = list(zipped_iterator)
 print(zipped_values)
+
+import math
+
+x = 0
+
+for k in range(8):
+    x += math.factorial(4 * k) * (1103 + 26390 * k) / (math.factorial(k) ** 4 * 396 ** (4 * k))
+    pi = (99 ** 2) / (2 * math.sqrt(2) * x)
+
+    print("Annäherung an pi mit {0}. Reihe : {1}").format(k, pi)
