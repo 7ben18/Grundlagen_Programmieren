ab228d740b15b077629074978cfe219213cf04e0
Ben Tran <66916399+7ben18@users.noreply.github.com>
2021-10-24 21:05:29 +0200

    add datetime

--- 01AufgabenSerien_primzahl/06Zeitmessung.py
+++ 01AufgabenSerien_primzahl/06Zeitmessung.py
@@ -1,24 +1,43 @@
+def eratosthenes(n):
+    zahlenliste = [z for z in range(2,n,1)]
+    primezahlen = []
+
+    for i in range(2,n,1):
+        if i in zahlenliste:
+            primezahlen.append(i)
+            zahlenliste.remove(i)
+        for a in zahlenliste:
+            if a % i == 0:
+                zahlenliste.remove(a)
+    return primezahlen
+
+def distances(primes):
+    distance = []
+    for x in range(0,len(primes)-1,1):
+        diff = (primes[x+1] - primes[x])
+        #diff = abs(diff)
+        distance.append(diff)
+    return distance
+
+def heuristic2(n):
+    thisdict = {
+    }
+    for i in n:
+        x = 0
+        for y in n:
+            if y == i:
+                x += 1
+        thisdict[i] = x
+    return thisdict
+
+
+
 import datetime
-
-def is_prime(n):
-    if n == 1:
-        return False
-    elif n == 2:
-        return True
-    else:
-        for i in range(2,n,1): # is is 2 until n
-            if n % i == 0: # 4 % 2 == 0 -> 4 is not a prime!
-                return False
-        else:
-            return True
-
-def all_primes_comprehension(n):
-    return [i for i in range(2,n) if is_prime(i)]
-
+from datetime import cnt
 
 start = datetime.datetime.now()
-print(all_primes_comprehension(1000))
+print(heuristic2(distances(eratosthenes(100))))
 dur = (datetime.datetime.now() - start)
+time_in_seconds = (dur.microseconds + 10 ** 6 * dur.total_seconds()) / cnt / 10 ** 6
 
-time_in_seconds = (dur.microseconds + 10 ** 6 * dur.total_seconds()) / 10 ** 6
-print(time_in_seconds)
+print(dur)
