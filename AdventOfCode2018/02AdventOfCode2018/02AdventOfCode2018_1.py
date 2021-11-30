a35c6cf61176ca5fc5d876ce4c2e47e2847221d1
Ben <si_ben_tran@hotmail.com>
2021-11-30 08:38:45 +0100

    add day 2 2018, not completed

--- AdventOfCode2018/02AdventOfCode2018/02AdventOfCode2018_1.py
+++ AdventOfCode2018/02AdventOfCode2018/02AdventOfCode2018_1.py
@@ -9,7 +9,22 @@
 # Daten einelsen
 with open("testdata_2018_2") as f:
     line = [str(x) for x in f.readlines()]
-    line = [x.strip().split() for x in line]
+    line = [x.strip() for x in line]
     print(line)
 
+    masterdict = {}
 
+
+    for i in range(len(line)):
+        dict = {}
+        cnt = 1
+        masterdict[i] = dict
+
+        for a in line[i]:
+            if a in dict:
+                cnt += 1
+                dict[a] = cnt
+            else:
+                dict[a] = cnt
+
+    print(masterdict)
