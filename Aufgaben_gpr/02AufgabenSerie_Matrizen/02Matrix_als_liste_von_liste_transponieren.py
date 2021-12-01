f537afaa008cf1ddf18c94b216d9933468afcf1a
Ben <si_ben_tran@hotmail.com>
2021-12-01 08:59:23 +0100

    add transponse3

--- Aufgaben_gpr/02AufgabenSerie_Matrizen/02Matrix_als_liste_von_liste_transponieren.py
+++ Aufgaben_gpr/02AufgabenSerie_Matrizen/02Matrix_als_liste_von_liste_transponieren.py
@@ -24,12 +24,17 @@ def transponse(n):
         matrix.append(q)
     return matrix
 
-print(transponse([[1, 0, 3], [0, 2, 4]]))
-print(transponse([[1, 0], [0, 2], [3, 4]]))
+#print(transponse([[1, 0, 3], [0, 2, 4]]))
+#print(transponse([[1, 0], [0, 2], [3, 4]]))
 
 def transponse2(n):
     nums = [n[s][i] for i in range(0, len(n[0]), 1) for s in range(0, len(n), 1)]
     return [nums[a:a+len(n)] for a in range(0,len(nums),2)]
 
 print(transponse2([[1, 0, 3], [0, 2, 4]]))
-print(transponse2([[1, 0], [0, 2], [3, 4]]))
+#print(transponse2([[1, 0], [0, 2], [3, 4]]))
+
+def transponse3(n):
+    return [[row[i] for row in n] for i in range(len(n[0]))]
+
+print(transponse3([[1, 0, 3], [0, 2, 4]]))
