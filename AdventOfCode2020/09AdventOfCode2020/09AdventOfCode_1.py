af97c5a11ddb3bd046dbfac72e29838b1b1f73d1
Ben <si_ben_tran@hotmail.com>
2021-12-07 16:00:44 +0100

    add aoc20 day9 task1&2

---
+++ AdventOfCode2020/09AdventOfCode2020/09AdventOfCode_1.py
@@ -0,0 +1,28 @@
+with open("09data") as f:
+    nums = [int(x) for x in f.readlines()]
+
+preamble = 25
+failure = 0
+rangefailure = 0
+
+for i in range(preamble,len(nums),1):
+    validate = False
+    for a in range(i-preamble,i,1):
+        for b in range(i-preamble, i, 1):
+            if nums[a] != nums[b]:
+                if nums[a] + nums[b] == nums[i]:
+                    validate = True
+    if validate == False:
+        failure = nums[i]
+        rangefailure = i
+
+# task 2
+numslist = nums[0:rangefailure]
+list = []
+
+for i in range(len(numslist)):
+    for a in range(i,len(numslist)):
+        if sum(numslist[i:a]) == failure:
+            list = numslist[i:a]
+print(max(list) + min(list))
+
