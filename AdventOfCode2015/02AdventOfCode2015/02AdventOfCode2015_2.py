5bed1dd94bffa34a48ec476b45ca9ea2c591f5c1
Ben Tran <66916399+7ben18@users.noreply.github.com>
2021-12-12 14:20:31 +0100

    add aoc15, day2 task 2

--- AdventOfCode2015/02AdventOfCode2015/02AdventOfCode2015_2.py
+++ AdventOfCode2015/02AdventOfCode2015/02AdventOfCode2015_2.py
@@ -1,3 +1,18 @@
-with open("02testdata") as f:
-    line = f.read().split("x")
-    print(line)
+with open("02data") as f:
+    line = [x.strip().split("x") for x in f.readlines()]
+    line2 = []
+    for x in line:
+        subline = []
+        for a in x:
+            subline.append(int(a))
+        sub = sorted(subline)
+        line2.append(sub)
+    #print(line2)
+
+    feet_of_ribbon = 0
+
+    for el in line2:
+        #print(el[0] * 2 + el[1] * 2)
+        #print(el[2] * el[0] * el[1])
+        feet_of_ribbon += el[0] + el[0] + el[1] + el[1] + (el[0] * el[1] * el[2])
+    print(feet_of_ribbon)
