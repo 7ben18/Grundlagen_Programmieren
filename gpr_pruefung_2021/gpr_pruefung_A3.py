4bfde907072fc736b90730573d81d164ece2151a
Ben <si_ben_tran@hotmail.com>
2022-01-17 13:48:59 +0100

    add next_prime()

--- gpr_pruefung_2021/gpr_pruefung_A3.py
+++ gpr_pruefung_2021/gpr_pruefung_A3.py
@@ -1,10 +1,24 @@
 # Aufgabe 3
 
 def is_prime(n):
-    for i in range(2,n):
+    for i in range(2, n // 2 + 1):
         if n % i == 0:
             return False
-        else:
-            return True
+    return True
 
-def next_prime
+
+print(is_prime(9))
+
+
+def next_prime(n):
+    num = n * 2 ** 2
+    numlst = [x for x in range(1, num)]
+
+    numlst = numlst[n:]
+
+    for i in numlst:
+        if is_prime(i) == True:
+            return i
+
+
+print(next_prime(8))
