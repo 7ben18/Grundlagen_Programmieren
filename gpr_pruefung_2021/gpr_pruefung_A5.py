8caccf70c0d6257b475832f54cdc6bc7be3073af
Ben <si_ben_tran@hotmail.com>
2022-01-17 15:33:22 +0100

    add uebungspruefung_A5

--- gpr_pruefung_2021/gpr_pruefung_A5.py
+++ gpr_pruefung_2021/gpr_pruefung_A5.py
@@ -2,7 +2,7 @@
 def is_ok(temperature, status):
     t = temperature
     s = status
-    if s == "ok" and t >= -10 and t <= 44:
+    if s == "ok" and t >= -10 and t <= 45:
         return True
     elif s == "off" and t == 0:
         return True
@@ -24,11 +24,21 @@ def count_errors(file_name):
         lst = [x.split() for x in f]
         cnt = 0
         for x in lst:
-            if is_ok(int(x[2][:-1]),x[3]) == False:
+            if not is_ok(int(x[2][:-1]),x[3]):
                 cnt += 1
         return cnt
 
 print(count_errors("gpr_pruefung_A5_datei"))
+
+def count_errors2(file_name):
+    with open(file_name) as f:
+        lst = [x.split() for x in f]
+        return len(list(filter(lambda x: not is_ok(int(x[2][:-1]),x[3]),lst)))
+
+def count_errors3(file_name):
+    with open(file_name) as f:
+        lst = [x.split() for x in f]
+        return sum([1 for x in lst if not is_ok(int(x[2][:-1]),x[3])])
 
 # Aufgabe 5c
 def count_errors_mult(file_names):
---
+++ gpr_pruefung_2021/gpr_pruefung_A5_1.py
@@ -0,0 +1,3 @@
+with open("gpr_pruefung_A5_datei") as f:
+    lst = [x.splitlines() for x in f]
+print(lst)
