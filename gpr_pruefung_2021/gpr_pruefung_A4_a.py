e46145a6224087d04ba654b477998d7e6a69dc6b
Ben <si_ben_tran@hotmail.com>
2022-01-17 17:07:23 +0100

    add pruefung A4 living neighbours

---
+++ gpr_pruefung_2021/gpr_pruefung_A4_a.py
@@ -0,0 +1,35 @@
+import copy
+
+grid = ['..........', '.###.##...', '.....##...', '.......##.', '.......##.', '..........']
+
+def living_neighbours(grid,row,col):
+    cnt = 0
+    if 0 < row and grid[row-1][col] == "#": # Oben
+        cnt += 1
+    if row < len(grid) and grid[row+1][col] == "#": # unten
+        cnt += 1
+    if 0 < col and grid[row][col-1] == "#": # Links
+        cnt += 1
+    if col < len(grid) and grid[row][col+1] == "#": # Rechts
+        cnt += 1
+    return cnt
+
+print(living_neighbours(grid, 1, 1))
+print(living_neighbours(grid, 1, 2))
+print(living_neighbours(grid, 3, 1))
+
+
+def step(grid):
+    new_grid = grid.copy()
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            neighbours = living_neighbours(grid, row, col)
+            if grid[row][col] == "." and neighbours == 2:
+                new_grid[row][col] = "#"
+            elif grid[row][col] == "#" and neighbours < 2 or neighbours > 3:
+                new_grid[row][col] = "."
+            else:
+                new_grid[row][col] = grid[row][col]
+    return new_grid
+
+print(step(grid))
