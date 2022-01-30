cf25a166cf39643293090c5ee4b2fd37507ea3c2
Ben <si_ben_tran@hotmail.com>
2022-01-30 09:05:30 +0100

    add gpr pruefung task 6

--- gpr_pruefung_2021/gpr_pruefung_A6.py
+++ gpr_pruefung_2021/gpr_pruefung_A6.py
@@ -1,14 +1,56 @@
-def collect_sales_data(file_name, customer_no):
-    with open(file_name) as f:
-        data = [x.strip().split() for x in f]
-        menge = 0
-        for y in data:
-            if customer_no == y[2]:
-                menge += y[5]
-    return menge
-print(collect_sales_data("gpr_pruefung_A6_datei", 12345))
+def collect_data_sales(file_name, customer_nr: int):
+    with open(file_name) as file:
+
+        # Kopfzeile einlesen
+        head = file.readline()
+        new_head = head.split("\\t")[:-1]
+
+        # Datensatz einlesen
+        rows = file.readlines()
+        new_rows = [x.split("\\t")[:-1] for x in rows]
+
+        #print(rows)
+        #print(new_rows)
+
+        # Kopfzeile analysieren
+        index_kunde = 0
+        index_artikel = 0
+        index_anzahl = 0
+
+        for index, col in enumerate(new_head):
+            if "Kunde" == col:
+                index_kunde = index
+                #print(index_kunde)
+            elif "Artikel" == col:
+                index_artikel = index
+                #print(index_artikel)
+            elif "Anzahl" == col:
+                index_anzahl = index
+                #print(index_anzahl)
+
+    # Datensatz analysieren
+    output = {}
+    for auftrag in new_rows:
+        if int(auftrag[index_kunde]) == customer_nr:
+            if auftrag[index_artikel] not in output:
+                output[auftrag[index_artikel]] = int(auftrag[index_anzahl])
+            else:
+                output[auftrag[index_artikel]] += int(auftrag[index_anzahl])
+    return output
+
+#print(collect_data_sales("gpr_pruefung_A6_datei", 12345))
+
+def print_sales_data(data):
+    print("Artikel -> Anzahl")
+    for key, value in data.items():
+        print(str(key) + " -> " + str(value))
+
+def articles(file_name, customer_no):
+    data = collect_data_sales(file_name, customer_no)
+    print_sales_data(data)
+
+articles("gpr_pruefung_A6_datei", 12345)
 
 
-with open("gpr_pruefung_A5_datei") as f:
-    x = [x.strip() for x in f]
-    print(x)
+
+# Merci Haris fuer dini hilf!
