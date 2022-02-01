52c42c7157c22cab4c79f7e7f60bd060119ddba5
Ben <si_ben_tran@hotmail.com>
2022-02-01 12:53:49 +0100

    add codewars needle in hystack

---
+++ Codewars/A_Needle_in_a_hystack.py
@@ -0,0 +1,21 @@
+"""
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+"""
+
+
+def find_needle(L: list) -> str:
+    for index, value in enumerate(L):
+        if value == "needle":
+            return "found the needle at position " +  str(index)
+
+print(find_needle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']))
+
+l = ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']
+for index, value in enumerate(l):
+    print(index, value)
