67d40ca6b23796a7a249bcf5d2ced6b28f98aa2d
Ben <si_ben_tran@hotmail.com>
2022-02-03 13:52:35 +0100

    add python comprehension

--- PracticePython/07list_comprehensions.py
+++ PracticePython/07list_comprehensions.py
@@ -1,27 +1,27 @@
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 100]
-
 b = [x for x in a if x % 2 == 0]
 print(b)
-
 # Weitere Comprehensions
 # comprehension obejct
 comobject = ((5 * x + 1 for x in a))
 print(comobject)
 print(list(comobject))
-
 # set comprehension
 setcomprehension = set((3 * x for x in a))
 print(setcomprehension)
-
 # tupple comprehension
 tupplecomprehension = tuple((2 * x for x in a if x % 2 != 0))
 print(tupplecomprehension)
-
 # dictionary comprehension
 dictcomprehension = {x:y for x in range(len(a)) for y in a}
 print(dictcomprehension)
-
 # Comprehension with sum
 a = [10, 10, 10, 20, 30, 40, 40, 10, 20]
 summeunique = sum((x for x in a if a.count(x) == 1))
 print(summeunique)
+
+# nested list comprehension
+for x in range(3):
+    for y in range(2):
+        for z in range(1):
+            print(x + y + z)
