0b3bbe83ae128e489e8e5c7895ceb91a5e604d55
Ben Tran <66916399+7ben18@users.noreply.github.com>
2022-02-03 18:48:10 +0100

    add remove_duplicated

--- Codewars/Remove_consecutive_duplicate_words.py
+++ Codewars/Remove_consecutive_duplicate_words.py
@@ -1,6 +1,10 @@
 def remove_consecutive_duplicates(s: str) -> str:
+    if len(s) < 1:
+        return ""
     new_word = ""
     s = s.split()
+    if len(s) < 1:
+        return s[0]
     for x in range(len(s) - 1):
         if s[x] != s[x + 1]:
             new_word += s[x] + " "
@@ -10,3 +14,14 @@ def remove_consecutive_duplicates(s: str) -> str:
 
 
 print(remove_consecutive_duplicates("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"))
+
+def remove_consecutive_duplicates2(w):
+    results = []
+    for word in w.split():
+        if word not in results:
+            results.append(word)
+        elif results[-1] != word:
+            results.append(word)
+    return " ".join(results)
+
+print(remove_consecutive_duplicates2("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"))
