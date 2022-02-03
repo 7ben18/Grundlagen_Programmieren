d7a493a18f93c829d312dde837018d03c7695f6b
Ben <si_ben_tran@hotmail.com>
2022-02-03 13:52:09 +0100

    add gpr pruefung A2

--- GPR_Test_2021/gpr_pruefung_A2.py
+++ GPR_Test_2021/gpr_pruefung_A2.py
@@ -1,6 +1,5 @@
 # Aufgabe 2
 lst1 = [x for x in range(10) if x % 3 == 0]
-print(lst1)
-
+print(lst1) # [0, 3, 6, 9]
 lst2 = [x ** 3 for x in range(10)]
-print(lst2)
+print(lst2) # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
