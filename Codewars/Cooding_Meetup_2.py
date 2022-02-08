1c124293e12d75964b09d91e06b921d254c2c6be
Ben Tran <66916399+7ben18@users.noreply.github.com>
2022-02-08 18:20:26 +0100

    add codewars

---
+++ Codewars/Cooding_Meetup_2.py
@@ -0,0 +1,46 @@
+"""
+You will be given an array of objects (associative arrays in PHP, tables in COBOL)
+representing data about developers who have signed up to attend the next coding
+meetup that you are organising.
+
+Your task is to return an array where each object will have a new property
+'greeting' with the following string value:
+
+Hi < firstName here >, what do you like the most about < language here >?
+
+For example, given the following input array:
+
+list1 = [
+  { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
+  { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
+  { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' }
+]
+your function should return the following array:
+
+[
+  { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java',
+    'greeting': 'Hi Sofia, what do you like the most about Java?'
+  },
+  { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python',
+    'greeting': 'Hi Lukas, what do you like the most about Python?'
+  },
+  { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby',
+    'greeting': 'Hi Madison, what do you like the most about Ruby?'
+  }
+]
+"""
+list1 = [
+  { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
+  { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
+  { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' }
+]
+
+def greet_developers(lst):
+    for key, value in enumerate(lst):
+        language = value["language"]
+        name = value["firstName"]
+        string = "Hi " + name + ", what do you like the most about " + language + "?"
+        value["greeting"] = string
+    return lst
+
+print(greet_developers(list1))
---
+++ Codewars/Cooding_Meetup_3.py
@@ -0,0 +1,25 @@
+"""
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+For example, given the following input array:
+"""
+
+list1 = [
+    { 'firstName': 'Sofia', 'lastName': 'I.', 'country': 'Argentina', 'continent': 'Americas', 'age': 35, 'language': 'Java' },
+    { 'firstName': 'Lukas', 'lastName': 'X.', 'country': 'Croatia', 'continent': 'Europe', 'age': 35, 'language': 'Python' },
+    { 'firstName': 'Madison', 'lastName': 'U.', 'country': 'United States', 'continent': 'Americas', 'age': 32, 'language': 'Ruby' }
+    ]
+
+def is_ruby_coming(lst):
+    ruby_cnt = 0
+    for index, value in enumerate(lst):
+        if value["language"] == "Ruby":
+            ruby_cnt += 1
+
+    return ruby_cnt > 0
+
+print(is_ruby_coming(list1))
